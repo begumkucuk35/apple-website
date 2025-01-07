7dd0f6cc0e45337efab85c7704e467c9fef02c87
begumkucuk35 <35begumkucuk@gmail.com>
2025-01-08 01:52:36 +0300

    videocarousel created

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { useState } from "react";
+import { hightlightsSlides } from "../constants";
 
 const VideoCarousel = () => {
-  return (
-    <div>VideoCarousel</div>
-  )
-}
+  const videoRef = useRef([]);
+  const videoSpanRef = useRef([]);
+  const videoDivRef = useRef([]);
 
-export default VideoCarousel
+  const [video, setVideo] = useState({
+    isEnd: false,
+    startPlay: false,
+    videoId: 0,
+    isLastVideo: false,
+    isPlaying: false,
+  });
+  const { isEnd, isLastVideo, startPlay, videoId, isPlaying } = video;
+  return (
+    <>
+      <div className="flex items-center">
+        {hightlightsSlides.map((list, i) => (
+          <div key={i} id="slider" className="sm:pr-20 pr-10">
+            <div className="video-carousel_container">
+              <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
+                <video
+                  id="video"
+                  className="pointer-events-none"
+                  preload="auto"
+                  muted
+                  playsInline={true}
+                >
+                  <source src={list.video} type="video/mp4" />
+                </video>
+              </div>
+              <div className="absolute top-12 left-[5%] z-10">
+                {list.textLists.map((text) => (
+                  <p key={text} className="md:text-2xl text-xl font-medium">
+                    {text}
+                  </p>
+                ))}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default VideoCarousel;
